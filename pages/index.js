2fad031a43f014ef67eb7de226c56a0ea4f3845b
moeez999 <moeezraza69@gmail.com>
2024-02-14 14:20:38 +0500

    code

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const IndexPage = () => {
+    return (
+        <div>
+            <h1>Welcome to Sees Redesign!</h1>
+        </div>
+    );
+};
+
+export default IndexPage;
