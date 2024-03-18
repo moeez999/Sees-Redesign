8aec694d244937fbc51416338434c89a5f13bd9e
moeez999 <moeezraza69@gmail.com>
2024-03-18 20:55:53 +0500

    faqlist update

--- pages/faqs.js
+++ pages/faqs.js
@@ -21,56 +21,87 @@ const Faqs = () => {
     <div>
       <Navigation />
 
-      <div className="my-28 flex flex-row gap-4 container mx-auto ">
-        <div className="container mx-auto px-4 py-8 max-w-xs sticky top-0 Shadow-custom h-screen ">
-          <h2 className="text-3xl text-secondary font-bold mt-8 mb-4">FAQs</h2>
+      <div className="my-28 flex flex-row Shadow-custom container mx-auto ">
+        <div className="container mx-auto px-4 bg-secondary py-8 max-w-xs sticky top-0  h-screen">
+          <div className="my-4">
+            <img src="../Images/faq.png" />
+          </div>
           <ul>
             <li
-              className="text-lg font-semibold text-primary my-1"
+              className="text-lg font-semibold text-white my-1 flex items-center gap-2"
               onClick={() => handleFaqsClick("BREEAM")}
             >
+              <img className="w-6" src="../White-Images/white_02-BREEAM.png" />
               BREEAM
             </li>
             <li
-              className="text-lg font-semibold text-primary my-1"
+              className="text-lg font-semibold text-white my-1 flex items-center gap-2"
               onClick={() => handleFaqsClick("BuildingLifeCycle")}
             >
+              <img
+                className="w-6"
+                src="../White-Images/white_10-Energy strategies for houses.png"
+              />
               Building Life Cycle
             </li>
             <li
-              className="text-lg font-semibold text-primary my-1"
+              className="text-lg font-semibold text-white my-1 flex items-center gap-2"
               onClick={() => handleFaqsClick("ThermalModelling")}
             >
+              <img
+                className="w-6"
+                src="../White-Images/white_09-overheating assessments.png"
+              />
               Thermal Modelling
             </li>
             <li
-              className="text-lg font-semibold text-primary my-1"
+              className="text-lg font-semibold text-white my-1 flex items-center gap-2"
               onClick={() => handleFaqsClick("ThermalBridging")}
             >
+              <img
+                className="w-6"
+                src="../White-Images/white_09-overheating assessments.png"
+              />
               Thermal Bridging
             </li>
             <li
-              className="text-lg font-semibold text-primary my-1"
+              className="text-lg font-semibold text-white my-1 flex items-center gap-2"
               onClick={() => handleFaqsClick("SapCalculations")}
             >
+              <img
+                className="w-6"
+                src="../White-Images/white_11 (SAP) -environmental performance certificates for houses.png"
+              />
               SAP Calculations
             </li>
             <li
-              className="text-lg font-semibold text-primary my-1"
+              className="text-lg font-semibold text-white my-1 flex items-center gap-2"
               onClick={() => handleFaqsClick("AirTightness")}
             >
+              <img
+                className="w-6"
+                src="../White-Images/white_01-air testing.png"
+              />
               AirTightness
             </li>
             <li
-              className="text-lg font-semibold text-primary my-1"
+              className="text-lg font-semibold text-white my-1 flex items-center gap-2"
               onClick={() => handleFaqsClick("WaterCalculations")}
             >
+              <img
+                className="w-6"
+                src="../White-Images/white_06-water calculations.png"
+              />
               Water Calculations
             </li>
             <li
-              className="text-lg font-semibold text-primary my-1"
+              className="text-lg font-semibold text-white my-1 flex items-center gap-2"
               onClick={() => handleFaqsClick("EnergyStrategy")}
             >
+              <img
+                className="w-6"
+                src="../White-Images/white_10-Energy strategies for houses.png"
+              />
               Energy Strategy
             </li>
             {/* Add more FAQ categories as needed */}
--- pages/services.js
+++ pages/services.js
@@ -9,7 +9,7 @@ const Service = () => {
       <div data-aos="fade-right" className="service-s">
         <Navigation />
       </div>
-      <div className="pt-28 services-bg absolute top-0 left-1/2 transform -translate-x-1/2 w-full  z-[-10]">
+      <div className="pt-28 absolute top-0 left-1/2 transform -translate-x-1/2 w-full  z-[-10]">
         <div className="flex justify-center flex-col items-center">
           <h1 className=" text-2xl md:text-4xl xxl:text-5xl xxxl:text-7xl font-bold  p-4 xxxl:p-8 animate__animated animate__lightSpeedInRight">
             <span className="text-secondary">Our</span> Services
